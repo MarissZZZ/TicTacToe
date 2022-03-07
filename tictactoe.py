1580d55198c0d4b6ae8e459258cb68345f329bd8
MarissZZZ <marisszuss@gmail.com>
2022-03-07 10:26:33 +0200

    GandrizGatavs

--- tictactoe.py
+++ tictactoe.py
@@ -26,7 +26,7 @@ def btnClick(button): #padod pogui
     return
 
 def checkWinner():
-    global checkWinner
+    global winner
     if (btn1["text"]=="X" and btn2["text"]=="X" and btn3["text"]=="X" or 
     btn1["text"]=="X" and btn4["text"]=="X" and btn7["text"]=="X" or
     btn4["text"]=="X" and btn5["text"]=="X" and btn6["text"]=="X" or
@@ -52,15 +52,15 @@ def checkWinner():
         messagebox.showinfo('TicTacToe', 'Spēles rezultāts ir neizšķirts')
     return
 
-btn1=Button(mansLogs,text=" ", width=6,height=3,font=('Helvica',24), command=lambda:btnClick(btn1))
-btn2=Button(mansLogs,text=" ", width=6,height=3,font=('Helvica',24), command=lambda:btnClick(btn2))
-btn3=Button(mansLogs,text=" ", width=6,height=3,font=('Helvica',24), command=lambda:btnClick(btn3))
-btn4=Button(mansLogs,text=" ", width=6,height=3,font=('Helvica',24), command=lambda:btnClick(btn4))
-btn5=Button(mansLogs,text=" ", width=6,height=3,font=('Helvica',24), command=lambda:btnClick(btn5))
-btn6=Button(mansLogs,text=" ", width=6,height=3,font=('Helvica',24), command=lambda:btnClick(btn6))
-btn7=Button(mansLogs,text=" ", width=6,height=3,font=('Helvica',24), command=lambda:btnClick(btn7))
-btn8=Button(mansLogs,text=" ", width=6,height=3,font=('Helvica',24), command=lambda:btnClick(btn8))
-btn9=Button(mansLogs,text=" ", width=6,height=3,font=('Helvica',24), command=lambda:btnClick(btn9))
+btn1=Button(mansLogs,text=" ", width=6,height=3,font=('Helvica',24), bd=10, bg='lightgrey', command=lambda:btnClick(btn1))
+btn2=Button(mansLogs,text=" ", width=6,height=3,font=('Helvica',24), bd=10, bg='white', command=lambda:btnClick(btn2))
+btn3=Button(mansLogs,text=" ", width=6,height=3,font=('Helvica',24), bd=10, bg='lightgrey', command=lambda:btnClick(btn3))
+btn4=Button(mansLogs,text=" ", width=6,height=3,font=('Helvica',24), bd=10, bg='white', command=lambda:btnClick(btn4))
+btn5=Button(mansLogs,text=" ", width=6,height=3,font=('Helvica',24), bd=10, bg='lightgrey', command=lambda:btnClick(btn5))
+btn6=Button(mansLogs,text=" ", width=6,height=3,font=('Helvica',24), bd=10, bg='white', command=lambda:btnClick(btn6))
+btn7=Button(mansLogs,text=" ", width=6,height=3,font=('Helvica',24), bd=10, bg='lightgrey', command=lambda:btnClick(btn7))
+btn8=Button(mansLogs,text=" ", width=6,height=3,font=('Helvica',24), bd=10, bg='white', command=lambda:btnClick(btn8))
+btn9=Button(mansLogs,text=" ", width=6,height=3,font=('Helvica',24), bd=10, bg='lightgrey', command=lambda:btnClick(btn9))
 
 btn1.grid(row=0,column=0)
 btn2.grid(row=0,column=1)
